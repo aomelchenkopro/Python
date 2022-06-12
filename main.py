9026f529551d8358a69485cb5118779319fc8f57
aomelchenko.pro@outlook.com <aomelchenko.pro@outlook.com>
2022-06-12 14:55:38 -0400

    Correct the code

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def parse(query: str) -> dict:
     return {}
 
 if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&age=purple') == {'name': 'ferret', 'age': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&age=purple&') == {'name': 'ferret', 'age': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
